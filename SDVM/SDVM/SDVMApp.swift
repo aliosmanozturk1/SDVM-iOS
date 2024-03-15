63ae52efb50052228b679dfadcd7e4e804db8708
Ali Osman Öztürk <developeralioz@gmail.com>
2024-03-15 15:27:57 +0300

    Update SDVMApp.swift

--- SDVM/SDVM/SDVMApp.swift
+++ SDVM/SDVM/SDVMApp.swift
@@ -1,17 +1,89 @@
-//
-//  SDVMApp.swift
-//  SDVM
-//
-//  Created by Ali Osman Öztürk on 15.03.2024.
-//
-
 import SwiftUI
+import Firebase
+
+/**
+ * App Delegate
+ */
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct SDVMApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @StateObject var sessionService = SessionServiceImpl()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            switch sessionService.state {
+                
+            case .loggedIn:
+                if (sessionService.userDetails?.types == "Çevirmen") {
+                    
+                    TabView {
+                        cevirmenView()
+                            .tabItem {
+                                Label("Ana Sayfa", systemImage: "house.fill")
+                            }
+                        
+                        HomeView()
+                            .environmentObject(sessionService)
+                            .tabItem {
+                                Label("Profil", systemImage: "person")
+                            }
+                        
+                    }
+                }
+                
+                if (sessionService.userDetails?.types == "Engelli") {
+                    TabView {
+                        engelliView()
+                            .tabItem {
+                                Label("Ana Sayfa", systemImage: "house.fill")
+                            }
+                        
+                        HomeView()
+                            .environmentObject(sessionService)
+                            .tabItem {
+                                Label("Profil", systemImage: "person")
+                            }
+                        
+                    }
+                }
+                
+                if (sessionService.userDetails?.types == "Takipçi") {
+                    TabView {
+                        takipciView()
+                            .tabItem {
+                                Label("Ana Sayfa", systemImage: "house.fill")
+                            }
+                        
+                        HomeView()
+                            .environmentObject(sessionService)
+                            .tabItem {
+                                Label("Profil", systemImage: "person")
+                            }
+                        
+                    }
+                }
+                
+                
+                
+            case .loggedOut:
+                LoginView()
+                
+            }
         }
     }
 }
+    
+    // HomeView()
+    //    .environmentObject(sessionService)
+
