561ab950dc364ae24290fa90911ee69549eec42e
Ali Osman Öztürk <developeralioz@gmail.com>
2024-03-15 15:18:34 +0300

    Add HomeView

---
+++ SDVM/SDVM/Feature/Home/Views/HomeView.swift
@@ -0,0 +1,43 @@
+//
+//  HomeView.swift
+//  Firebase User Account Management
+//
+//  Created by Tunde on 22/05/2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @EnvironmentObject var service: SessionServiceImpl
+    
+    var body: some View {
+            VStack(alignment: .leading,
+                   spacing: 16) {
+                
+                VStack(alignment: .leading,
+                       spacing: 16) {
+                    Text("Ad: \(service.userDetails?.firstName ?? "N/A")")
+                    Text("Soyad: \(service.userDetails?.lastName ?? "N/A")")
+                    Text("Telefon: \(service.userDetails?.occupation ?? "N/A")")
+                    Text("Cinsiyet: \(service.userDetails?.gender ?? "N/A")")
+                }
+                
+                
+                    
+                    ButtonView(title: "Hesaptan Çıkış Yap") {
+                        service.logout()
+                    }
+                
+            }
+            .padding(.horizontal, 16)
+            .navigationTitle("Main ContentView")        
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+            HomeView()
+                .environmentObject(SessionServiceImpl())
+    }
+}
