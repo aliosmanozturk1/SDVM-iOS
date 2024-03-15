fdc1a30f653cf1f9cd24df4b4584b9fe2f10522c
Ali Osman Öztürk <developeralioz@gmail.com>
2024-03-15 15:08:32 +0300

    Create Project

---
+++ SDVM/SDVM/SDVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDVMApp.swift
+//  SDVM
+//
+//  Created by Ali Osman Öztürk on 15.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SDVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
